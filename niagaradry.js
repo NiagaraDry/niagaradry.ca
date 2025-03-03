d2ca54f46cfd7b820f0a410560ee71a8c3a9cb07
NiagaraDry <niagaracollector@gmail.com>
2025-03-03 18:18:56 -0500

    forgot to stage this lol

---
+++ niagaradry.js
@@ -0,0 +1,27 @@
+function openNav() {
+	document.getElementById("mySidebar").style.display = "block";
+	document.getElementById("myOverlay").style.display = "block";
+}
+ 
+function closeNav() {
+	document.getElementById("mySidebar").style.display = "none";
+	document.getElementById("myOverlay").style.display = "none";
+}
+
+function onClick(element) {
+	/* Modified version for thumbnail support once it's ready */
+	/*
+	var url = element.src.replace("images/min/" , "images/");
+	document.getElementById("img01").src = url;
+	document.getElementById("modal01").style.display = "block";
+	var captionText = document.getElementById("caption");
+	captionText.innerHTML = element.alt;
+	*/
+
+	/* Original code below */
+	var url = document.getElementById("img01").src;
+	document.getElementById("img01").src = element.src;
+	document.getElementById("modal01").style.display = "block";
+	var captionText = document.getElementById("caption");
+	captionText.innerHTML = element.alt;
+}
